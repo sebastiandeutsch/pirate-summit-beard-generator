256069c3e7da17e958680aad6b3478390c8f6e3b
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2015-10-06 18:40:52 +0200

    Created a page to show all the images.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,19 @@
 class ImagesController < ApplicationController
   layout 'print'
 
+  def index
+    @images = Image.all.where(dontshow: false)
+  end
+
   def show
     @image = Image.find(params[:id])
+  end
+
+  def dontshow
+    @image = Image.find(params[:id])
+    @image.dontshow = true
+    @image.save
+
+    redirect_to images_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   post '/upload' => 'application#upload'
 
-  resources :images, only: [:show]
+  resources :images, only: [:show, :index] do
+    member do
+      get 'dontshow'
+    end
+  end
 
   root 'application#index'
 end
